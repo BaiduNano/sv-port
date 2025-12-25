ca3c596e1383b37923ef0be8a51ac94620f04692
Baidu <baidunano@proton.me>
2025-12-25 17:56:21 +0700

    fix asset path

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 		}),
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '/sv-port' : '',
-			assets: 'https://baidunano.github.io/sv-port'
+			assets: 'https://raw.githubusercontent.com/BaiduNano/sv-port/refs/heads/gh-pages'
 		}
 	}
 };
