afeb88297e4356c786a38230f8eff114d05e1323
Baidu <baidunano@proton.me>
2025-12-25 17:22:05 +0700

    Update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="bg-gray-950 w-screen h-screen">
+    <div class="flex justify-center w-full h-full">
+        <span class="self-center">
+            <h1 class="text-white font-black text-6xl">Hello.</h1>
+            <h1 class="text-white">BaiduNano</h1>
+        </span>
+    </div>
+</div>
