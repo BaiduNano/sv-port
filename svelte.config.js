bf22209e0908b3e41874a6bae843f65e21a6f303
Baidu <baidunano@proton.me>
2025-12-25 14:57:36 +0700

    first commit

---
+++ svelte.config.js
@@ -0,0 +1,24 @@
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	}
+};
+config.paths = { base: process.argv.includes('dev') ? '' : "/sv-port" }
+
+export default config;
