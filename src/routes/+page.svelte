372bf697a43cea1a3fffdfdbd9d469a195e14a66
Baidu <baidunano@proton.me>
2026-06-30 10:32:48 +0700

    added sum bs

---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+    import { onMount } from "svelte";
+
+    const links = [
+        "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        "https://www.youtube.com/watch?v=jAGCnOi3gt0",
+        "https://www.youtube.com/watch?v=FumbPNRKTs8",
+        "https://www.youtube.com/watch?v=uZeXQeyv-VE",
+        "https://www.youtube.com/watch?v=TGNvRJX3Gu8",
+        "https://www.youtube.com/watch?v=54yKPwskCBg",
+        "https://www.youtube.com/watch?v=Zh6J2b2MZHw",
+        "https://www.youtube.com/watch?v=L2orcrXZDoM",
+        "https://www.youtube.com/watch?v=IVwJqEXWSOQ",
+        "https://www.youtube.com/watch?v=biUwIqFr4FM",
+        "https://www.youtube.com/watch?v=K0RcYkFY-YE",
+        "https://www.youtube.com/watch?v=1xtFp2Wu_To",
+        "https://www.youtube.com/watch?v=3hVXt9NYiJA",
+        "https://www.youtube.com/watch?v=p-v1Hn_aZHA",
+        "https://www.youtube.com/watch?v=EHpUtZ7yiYc",
+        "https://www.youtube.com/watch?v=ERCqmI3WLKQ",
+        "https://www.youtube.com/watch?v=BJLFfkGHWUU",
+        "https://www.youtube.com/watch?v=oOIztBXox60",
+        "https://www.youtube.com/shorts/z9Bp-4yIMvI",
+        "https://www.youtube.com/shorts/UYMCPfkS5Gs",
+        "https://www.youtube.com/watch?v=7dS5y4rJG04",
+    ];
+
+    onMount(() => {
+        const randomLink = links[Math.floor(Math.random() * links.length)];
+        window.location.href = randomLink;
+    });
+</script>
--- src/routes/home/+layout.svelte
+++ src/routes/home/+layout.svelte
@@ -1,12 +1,37 @@
 <script lang="ts">
-    import { darkMode } from "$lib/store/store";
-    import Particles from "$lib/components/particles.svelte";
-    import DarkModeToggler from "./darkModeToggler/darkModeToggler.svelte";
     import "../layout.css";
     import "../icons.css";
-
+    import { darkMode } from "$lib/store/store";
+    import { name as title } from "$lib/socialLinks/socialLinks";
+    import Particles from "$lib/components/particles.svelte";
+    import DarkModeToggler from "./darkModeToggler/darkModeToggler.svelte";
+    let description = "BaiduNano's personal website.";
+    let url = "https://baidunano.my/home";
+    let imageUrl =
+        "https://isexuebuwdjrzpwwssal.supabase.co/storage/v1/object/public/portfolio-images/ss.png";
     let { children } = $props();
 </script>
+
+<svelte:head>
+    <title>{title}</title>
+    <link
+        rel="stylesheet"
+        href="https://www.nerdfonts.com/assets/css/webfont.css"
+    />
+    <meta name="description" content={description} />
+
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content={url} />
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+    <meta property="og:image" content={imageUrl} />
+
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:url" content={url} />
+    <meta name="twitter:title" content={title} />
+    <meta name="twitter:description" content={description} />
+    <meta name="twitter:image" content={imageUrl} />
+</svelte:head>
 
 <div
     class="bg-base-300 text-base-content duration-200 w-screen h-screen relative overflow-hidden"
